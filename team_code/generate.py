415767dc76578ea470736af113b453db89fa29fd
Serge Gotsuliak <sgotsulyak@gmail.com>
2023-11-09 12:23:52 +0300

    ...

--- team_code/generate.py
+++ team_code/generate.py
@@ -279,7 +279,7 @@ def generate_text(model, tokenizer, cur_query_list, history_tensor=None):
     #history_tensor = torch.concat([history_tensor, prompt], dim=1)
     history_tensor[0][num]["embd"] = torch.concat([history_tensor[0][num]["embd"], prompt], dim=1)
 
-    return response, history_tensor
+    return response, history_tensor[0]
 
 
 # --- PPL ---
